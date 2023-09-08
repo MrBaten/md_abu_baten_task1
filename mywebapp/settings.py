cfd596f1ea9f7b3df1950a260c3a32f2b6c0ea21
Mr Baten <84617592+MrBaten@users.noreply.github.com>
2023-09-08 06:57:19 +0600

    re3

--- mywebapp/settings.py
+++ mywebapp/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = os.environ.get("SECRET_KEY")
 DEBUG = os.environ.get("DEBUG","False").lower() == "true"
 
 #ALLOWED_HOSTS = os.environ.get("ALLOWED_HOSTS").split(" ")
-ALLOWED_HOSTS = os.environ.get("ALLOWED_HOSTS".split(" "))
+ALLOWED_HOSTS = os.environ.get("ALLOWED_HOSTS").split(" ")
 # Application definition
 
 INSTALLED_APPS = [
